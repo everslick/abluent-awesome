016989df6487b114df2f011d3f12b179d5b7eb5e
Clemens Kirchgatterer <clemens@1541.org>
2015-09-08 23:31:48 +0200

    added --no-backup flag

--- install.sh
+++ install.sh
@@ -10,13 +10,16 @@ SRC=`pwd`
 echo
 echo "Installing Awesome config..."
 cd ~/.config/
-if [ -e awesome ] ; then
+if [ "$1" != "--no-backup" ] && [ -e awesome ] ; then
     echo "  renamimg original awesome directory to awesome.$BAK"
     mv awesome awesome.$BAK
+else
+    echo "  removing old awesome directory"
+    rm -rf awesome
 fi
 echo "  copying new files ..."
-cp -r $SRC ./awesome
+cp -r $SRC awesome
 echo "  deleting unneeded files ..."
-cd ./awesome && rm -rf .git* install.sh
+cd awesome && rm -rf .git* install.sh
 echo "done."
 echo
